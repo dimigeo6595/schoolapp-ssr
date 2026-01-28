f83ce15332468fe978630701b7943905b0f87475
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-01-28 20:00:26 +0200

    feat: add project initialization

---
+++ src/main/java/gr/aueb/cf/schoolapp/SchoolappApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.schoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolappApplication.class, args);
+	}
+
+}
