45b7e33951f017ec59df7615e6f7215b8dbe5b04
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-02-03 20:58:55 +0200

    feat: add features for teachers insert

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -3,11 +3,16 @@ package gr.aueb.cf.schoolapp.controller;
 
 import gr.aueb.cf.schoolapp.dto.RegionReadOnlyDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,6 +26,25 @@ public class TeacherController {
         return "teacher-insert";
     }
 
+    @PostMapping("/insert")
+    public String teacherInsert(@Valid TeacherInsertDTO teacherInsertDTO,
+                                BindingResult bindingResult,
+                                Model model,
+                                RedirectAttributes redirectAttributes){
+        if(bindingResult.hasErrors()){
+            return "teacher-insert";
+        }
+
+        //save teacher to DB
+        TeacherReadOnlyDTO teacherReadOnlyDTO = new TeacherReadOnlyDTO("afc-1234", "Alice", "Wonderland");
+        // model.addAttribute("teacherReadOnlyDTO", teacherReadOnlyDTO);
+
+
+        //RPG - Post-Redirect-Get
+        redirectAttributes.addFlashAttribute("teacherInsertDTO", teacherInsertDTO);
+        return "redirect:/teachers/success";
+    }
+
 
     @ModelAttribute("regionsReadOnlyDTO")
     public List<RegionReadOnlyDTO> regions(){
@@ -30,4 +54,6 @@ public class TeacherController {
                 new RegionReadOnlyDTO(3L, "Thessaloniki")
         );
     }
+
+
 }
